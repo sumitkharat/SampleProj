29fd681d6f9c418fb7ad75ea40fd7ab9e7c6f722
Sumit.dev <sumitkharat.dev@gmail.com>
2024-12-05 20:47:11 +0530

    Added one controller also

--- src/main/java/com/sk/controller/SampleController.java
+++ src/main/java/com/sk/controller/SampleController.java
@@ -3,6 +3,7 @@ package com.sk.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
 	/// sk/sample/name
-	
+
 	@GetMapping("/name")
 	public ResponseEntity<String> sample() {
 		return new ResponseEntity<String>("Sumit-Kharat", HttpStatus.OK);
+	}
+
+	@GetMapping("/name/{name}")
+	public ResponseEntity<String> sample(@PathVariable("name") String name) {
+		return new ResponseEntity<String>(name, HttpStatus.OK);
 	}
 }
